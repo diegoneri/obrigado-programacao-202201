3253a51be4157c216d3f84788b084fd569463b11
Diego Neri <diego@diegoneri.com>
2022-06-03 19:40:23 -0300

    Finaliza programa

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Figgle;
+
+Console.BackgroundColor = ConsoleColor.DarkGreen;
+Console.ForegroundColor = ConsoleColor.Black;
+
+Console.WriteLine(
+    FiggleFonts.Standard.Render("Obrigado!"));
+
+Console.BackgroundColor = ConsoleColor.DarkBlue;
+
+Console.WriteLine(
+    FiggleFonts.Big.Render("Java"));
+
+Console.ResetColor();
+
+Console.WriteLine("Pressione uma tecla para finalizar...");
+
+Console.ReadKey();
